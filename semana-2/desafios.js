d21cc6c3c175a138f44f7e8abc9f952dd7e9811a
Diogo Moura <diogoodev@gmail.com>
2024-04-07 23:22:45 -0300

    Create desafios.js

---
+++ semana-2/desafios.js
@@ -0,0 +1,27 @@
+//Crie uma lista vazia, com o nome listaGenerica.
+
+let listaGenerica = [];
+
+//Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+
+let linguagensDeProgramacao = ["javaScript","C","C++","Kotlin","Python"]
+
+//Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+
+linguagensDeProgramacao.push("Java")
+linguagensDeProgramacao.push("Ruby")
+linguagensDeProgramacao.push("GoLang")
+
+
+//Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+
+let frutas = ["uva", "maçã", "banana"]
+console.log(frutas[0])
+
+//Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+
+console.log(frutas[2])
+
+//Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+
+console.log(frutas[frutas.length - 1])
